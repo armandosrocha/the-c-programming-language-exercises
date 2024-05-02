3b8bf0612b366a078bc6a3d6d219b55ccc0c09ef
Armando Rocha <armando.sv.rocha@gmail.com>
2024-05-02 16:36:21 -0300

    write a program to print the corresponding celsius-to-farh table

---
+++ tcpl-examples/c1/celsius-to-farh.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/*print Fahrenheit-Celsius table
+ for fahr = 0, 20, ..., 300 */
+int main() {
+  // int fahr, celsius;
+  // int lower, upper, step;
+
+  // using floats now
+  float fahr, celsius;
+  float lower, upper, step;
+
+  lower = 0;   /*lower limit of temperature scale*/
+  upper = 300; /* upper limit */
+  step = 20;   /* step size */
+
+  fahr = lower;
+
+  printf(" ºC     ºF\n ---------\n");
+  while (fahr <= upper) {
+    // celsius = 5 * (fahr - 32) / 9;
+    // printf("%3d%6d\n", fahr, celsius);
+    // fahr = fahr + step;
+
+    // Add float number
+    celsius = (5.0 / 9.0) * (fahr - 32.0);
+    printf("%3.1f %6.0f\n", celsius, fahr);
+    fahr = fahr + step;
+  }
+}
